ca06976d1d108cf51744c8c9e3c4c37204b5fe65
alaynesigua <115857320+alaynesigua@users.noreply.github.com>
2023-10-18 17:21:50 +0800

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('indexs');
+});
 Route::get('indexs', function () {
     return view('indexs');
 });
